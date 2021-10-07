014e8775fd71c82647615e2aaafe11e95052cc78
albert <albert.perlman.dev@gmail.com>
2021-10-06 18:48:48 -0600

    GPU v0.0

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -21,47 +21,32 @@ class MainWindow(QMainWindow):
 
     self.appctxt = ApplicationContext()
 
-    # Main Widget Container
     MainWidgetContainer = QWidget()
     QFontDatabase.addApplicationFont(self.appctxt.get_resource('fonts/Montserrat-Thin.ttf'))
-    # MainWidgetContainer.setStyleSheet(StyleSheet.css("window"))
     self.setCentralWidget(MainWidgetContainer)
 
-    windowPosX = 3600
-    windowPosY = -500
-    windowWidth = 160
-    windowHeight = 180
-
-    timer = QTimer(self)                 # create a timer object
-    timer.timeout.connect(self.update)   # add action to the timer, update the whole code
-    timer.start(0)                    # update cycle in milliseconds
-
-    self.setGeometry(windowPosX, windowPosY, windowWidth, windowHeight)       # window location and size
+    # configure application window
+    self.setFixedSize(160, 180)
     self.setAttribute(Qt.WA_TranslucentBackground)
-    self.setWindowFlags(Qt.FramelessWindowHint)
+    self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool)
+
+    # gauge refresh rate (queues self.paintEvent every 500ms)
+    refreshTimer = QTimer(self)
+    refreshTimer.timeout.connect(self.update)
+    refreshTimer.start(500)
 
     #############
     #  WIDGETS  #
     #############
-    # # close button
-    # self.closeBtn = QPushButton(MainWidgetContainer)
-    # self.closeBtn.setFixedSize(40,40)
-    # self.closeBtn.setText('X')
-    # self.closeBtn.setStyleSheet(StyleSheet.css("closeFocusIn"))
-    # self.closeBtn.clicked.connect(lambda: self.close())
-    # pos = QPoint(windowWidth-30,-10)
-    # self.closeBtn.move(pos)
-
-    spacerTop = QLabel()
+    spacerTop = QWidget()
     spacerTop.setFixedSize(0,15)
+    spacerBottom = QWidget()
+    spacerBottom.setFixedSize(0,2.5)
 
     # temp display
     self.tempDisplay = QLabel(str(self.getGpuTemp()))
     self.tempDisplay.setAlignment(Qt.AlignCenter)
     self.tempDisplay.setStyleSheet(StyleSheet.css("temp"))
-
-    spacerBottom = QLabel()
-    spacerBottom.setFixedSize(0,2.5)
 
     # GPU label
     gpuLabel = QLabel("GPU")
@@ -82,17 +67,14 @@ class MainWindow(QMainWindow):
     ####################
     #  SIGNAL / SLOTS  #
     ####################
-    self.focusIn.connect(self.SLOT_focusIn)
-    self.focusOut.connect(self.SLOT_focusOut)
 
     ####################
     #     START-UP     #
     ####################
-    self.updateTitle()
     self.show()
-    self.setFocusPolicy(Qt.ClickFocus)
 
-### end MainWindow init() #####################################################################
+
+# -- methods
 
   def getGpuTemp(self):
     clr.AddReference(r"D:\util\OpenHardwareMonitor\OpenHardwareMonitorLib.dll")
@@ -107,64 +89,61 @@ class MainWindow(QMainWindow):
         c.Hardware[1].Update()
         return int(c.Hardware[1].Sensors[a].get_Value())
 
+
+  # draw gauge, update temp display, and set window position
   def paintEvent(self, event):
+
+    # set display temperature
+    gpuTemp = self.getGpuTemp()
+    self.tempDisplay.setText(str(gpuTemp))
+
+    # gauge painter canvas size
     canvasHeight = 150
     canvasWidth = canvasHeight
 
+    # gauge arc range: [0-270] degrees
     gaugeSize = 270
     gaugeWidth = 5
     gaugeOutlineWidth = 0.2
 
+    # temp range: [20 - 100] C
+    minTemp = 20
+    maxTemp = 100
+
+    if gpuTemp < 20: gpuTemp = 20
+    if gpuTemp > 100: gpuTemp = 100
+
+    # map hardware temp reading from temp range [20-100] to gauge range [0-270]
+    gaugeValue = (((gpuTemp - minTemp) * (gaugeSize)) / (maxTemp - minTemp))
+
+    # paint gauge value
     gaugeValuePainter = QPainter(self)
     gaugeValuePainter.setRenderHint(QPainter.Antialiasing)
     gaugeValuePainter.setPen(QPen(Qt.white, gaugeWidth, cap=Qt.SquareCap))
 
-    # # ---------- the following lines simulate sensor reading. -----------
-    # gaugeValue = 0
-    # adder = 0.1
-    # if gaugeValue > gaugeSize or gaugeValue < 0:  # variable to make arc move
-    #     adder = -adder  # gaugeValue corresponds to the
-    #     # value to be indicated by the arc.
-    # gaugeValue = gaugeValue + adder
-    # # --------------------- end simulation ------------------------------
-
-    minTemp = 20
-    maxTemp = 100
-    gpuTemp = self.getGpuTemp()
-
-    # map temp range [20-100] to gauge range [0-270]
-    gaugeValue = (((gpuTemp - minTemp) * (gaugeSize)) / (maxTemp - minTemp))
-    self.tempDisplay.setText(str(gpuTemp))
-
-    # drawArc syntax:
-    #       drawArc(canvas_origin_x, canvas_origin_y, canvas_width, canvas_height, startAngle, spanAngle)
+    # drawArc( canvas_origin_x, canvas_origin_y, canvas_width, canvas_height, start_angle, span_angle )
     gaugeValuePainter.drawArc( gaugeWidth/2, gaugeWidth/2,
-                     canvasHeight + gaugeWidth, canvasWidth + gaugeWidth,
-                     int((gaugeSize + (180 - gaugeSize) / 2)*16), int(-gaugeValue*16) )
+                               canvasHeight + gaugeWidth, canvasWidth + gaugeWidth,
+                               int((gaugeSize + (180 - gaugeSize) / 2)*16), int(-gaugeValue*16) )
+    gaugeValuePainter.end()
 
-
-    gaugeOutlinePainter = QPainter(self)    # create a painter object
-    gaugeOutlinePainter.setRenderHint(QPainter.Antialiasing)  # tune up painter
+    # paint gauge outline
+    gaugeOutlinePainter = QPainter(self)
+    gaugeOutlinePainter.setRenderHint(QPainter.Antialiasing)
     gaugeOutlinePainter.setPen(QPen(Qt.white, gaugeOutlineWidth, cap=Qt.FlatCap))
 
-    gaugeOutlinePainter.drawArc(gaugeWidth/2, gaugeWidth/2,   # binding box: x0, y0, pixels
-              canvasHeight + gaugeWidth, # binding box: height
-              canvasWidth + gaugeWidth,  # binding box: width
-              int((gaugeSize + (180 - gaugeSize) / 2)*16),  # arc start point, degrees (?)
-              int(-270*16))         # arc span
-
-    gaugeValuePainter.end()
+    gaugeOutlinePainter.drawArc( gaugeWidth/2, gaugeWidth/2,
+                                 canvasHeight + gaugeWidth, canvasWidth + gaugeWidth,
+                                 int((gaugeSize + (180 - gaugeSize) / 2)*16), int(-270*16) )
     gaugeOutlinePainter.end()
 
-  # SLOT: Main Window has been resized
-  def SLOT_focusIn(self):
-    self.closeBtn.setStyleSheet(StyleSheet.css("closeFocusIn"))
-    print("focus in")
+    self.setWindowPos()
 
-  # SLOT: Main Window has been resized
-  def SLOT_focusOut(self):
-    self.closeBtn.setStyleSheet(StyleSheet.css("closeFocusOut"))
-    print("focus out")
+
+  def setWindowPos(self):
+    configFile = open(self.appctxt.get_resource('gauge.config'), 'r')
+    windowPos = list(map(int, configFile.readline().split(',')))
+    self.move(windowPos[0],windowPos[1])
 
 
   # critical dialog pop-up
@@ -174,16 +153,6 @@ class MainWindow(QMainWindow):
     dlg.setIcon(QMessageBox.Critical)
     dlg.show()
 
-  # delete all widgets in a layout
-  def clearLayout(self, layout):
-    while layout.count():
-      child = layout.takeAt(0)
-      if child.widget():
-        child.widget().deleteLater()
-
-  # update main window title
-  def updateTitle(self, str=""):
-    self.setWindowTitle(""+ str)
 
   # map key press events to gallery navigation
   def keyPressEvent(self, event):
@@ -220,43 +189,10 @@ class MainWindow(QMainWindow):
     except:
       pass
 
-  # overload Main Window resizeEvent to emit signal
-  def resizeEvent(self, event):
-    self.resized.emit()
-    return super(MainWindow, self).resizeEvent(event)
 
-  # overload Main Window focusInEvent to emit signal
-  def focusInEvent(self, event):
-    print("focus in")
-    self.focusIn.emit()
-    return super(MainWindow, self).focusInEvent(event)
-
-  # overload Main Window focusOutEvent to emit signal
-  def focusOutEvent(self, event):
-    print("focus out")
-    self.focusOut.emit()
-    return super(MainWindow, self).focusOutEvent(event)
-
-def changedFocusSlot(old, now):
-    if (now==None and QApplication.activeWindow()!=None):
-        print("set focus to the active window")
-        QApplication.activeWindow().setFocus()
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    # QMainWindow.connect(appctxt, SIGNAL("focusChanged(QWidget *, QWidget *)"), changedFocusSlot)
     Window = MainWindow()
     exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
     sys.exit(exit_code)
-
-
-
-# class MainWindow(QMainWindow):
-#   focusIn = pyqtSignal()
-#   focusOut = pyqtSignal()
-#   def focusInEvent(self, event):
-#     print("focus in")
-#     self.focusIn.emit()
-#   def focusOutEvent(self, event):
-#     print("focus out")
-#     self.focusOut.emit()
