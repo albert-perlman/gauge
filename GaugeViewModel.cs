ac0907c698d0582360a00b88a63f058225f536a1
Albert Perlman <albert.perlman.dev@gmail.com>
2022-02-28 15:52:40 -0700

    WIP

---
+++ GaugeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauge
+{
+  public class GaugeViewModel
+  {
+    int arcLength_;
+    string temp_ = "0";
+    public GaugeViewModel()
+    {
+    }
+    public string Temp
+    {
+      get { return temp_; }
+      set { Temp = temp_; }
+    }
+
+  }
+}
