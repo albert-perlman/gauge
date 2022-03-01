4a63ddd47fe5c159e171966732e4028d3fffc5ab
Albert Perlman <albert.perlman.dev@gmail.com>
2022-03-01 16:13:32 -0700

    WIP

--- GaugeViewModel.cs
+++ GaugeViewModel.cs
@@ -1,23 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 namespace Gauge
 {
   public class GaugeViewModel
   {
     int arcLength_;
+    int radius_ = 150;
+    Point center_ = new(200,200);
     string temp_ = "0";
+    Point arcEnd_ = new(210,275);
     public GaugeViewModel()
     {
+      arcEnd_.X = center_.X + (radius_ * Math.Cos(Math.PI));
+      arcEnd_.Y = center_.Y + (radius_ * Math.Sin(Math.PI));
     }
     public string Temp
     {
       get { return temp_; }
       set { Temp = temp_; }
+    }
+    public Point ArcEnd
+    {
+      get { return arcEnd_; }
+      set { ArcEnd = arcEnd_; }
     }
 
   }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Printing;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Brushes = System.Windows.Media.Brushes;
+using Point = System.Windows.Point;
+using Size = System.Windows.Size;
 
 namespace Gauge
 {
@@ -24,6 +20,63 @@ namespace Gauge
     {
       InitializeComponent();
       DataContext = new GaugeViewModel();
+      
+      Canvas canvas = new Canvas();
+
+      var g = new StreamGeometry();
+      var g1 = new StreamGeometry();
+
+      using (var gc = g.Open())
+      {
+        gc.BeginFigure(
+            startPoint: new Point(90, 275),
+            isFilled: false,
+            isClosed: false);
+
+        gc.ArcTo(
+            point: new Point(210, 275),
+            size: new Size(100, 100),
+            rotationAngle: 0d,
+            isLargeArc: true,
+            sweepDirection: SweepDirection.Clockwise,
+            isStroked: true,
+            isSmoothJoin: false);
+      }
+
+      var path = new Path
+      {
+        Stroke = Brushes.Cyan,
+        StrokeThickness = 5,
+        Data = g
+      };
+      canvas.Children.Add(path);
+      using (var gc = g1.Open())
+      {
+        gc.BeginFigure(
+            startPoint: new Point(90, 275),
+            isFilled: false,
+            isClosed: false);
+
+        gc.ArcTo(
+            point: new Point(210, 20),
+            size: new Size(100, 100),
+            rotationAngle: 0d,
+            isLargeArc: true,
+            sweepDirection: SweepDirection.Clockwise,
+            isStroked: true,
+            isSmoothJoin: false);
+      }
+
+      var path1 = new Path
+      {
+        Stroke = Brushes.Red,
+        StrokeThickness = 5,
+        Data = g1
+      };
+      canvas.Children.Add(path1);
+      canBackArea.Children.Add(canvas);
     }
   }
+
+
 }
